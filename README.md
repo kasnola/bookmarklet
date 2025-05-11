843d70f82fc24badde7dc59db3fa72b6e899a034
kasnola <kasnola@hackclub.app>
2025-05-10 19:41:20 -0500

    OK, injecting an iframe works! sometimes!

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # bookmarklet
+
 a bookmarklet for the Hacklet YSWS that erases clutter, leaving just the &lt;article>
+
+## sites tested w/ iframe injection
+
+- theatlantic.com
+  - works well!
+- developer.mozilla.org
+  - refuses to connect
+- stackoverflow.com
+  - JANK and UNNECESSARY!
+- owl.purdue.edu
+  - JANK and UNNECESSARY!
--- main.js
+++ main.js
@@ -1,20 +1,53 @@
 // if (window.confirm("Do you want me to try"))
 // https://www.theatlantic.com/technology/archive/2022/12/chatgpt-ai-writing-college-student-essays/672371/
+
 javascript: (function () {
-  let peskyTagCount = 0;
+  function injectIframe() {
+    let iframe = document.createElement("iframe");
 
-  let peskySemanticTags = document.querySelectorAll("aside, footer, header");
+    iframe.setAttribute("src", document.URL);
+    iframe.setAttribute("sandbox", "");
+    iframe.setAttribute("width", "100%");
+    iframe.setAttribute("height", "100%");
+    iframe.setAttribute("frameborder", "0");
+    iframe.setAttribute("marginheight", "0");
+    iframe.setAttribute("marginwidth", "0");
+    iframe.setAttribute("scrolling", "auto");
 
-  console.log(peskySemanticTags);
+    document.body.innerHTML = "";
+    document.documentElement.appendChild(iframe);
 
-  peskySemanticTags.forEach((element) => {
-    peskyTagCount++;
-    element.remove();
-  });
+    document.documentElement.style.overflow = "auto";
+    document.documentElement.style.height = "100%";
+    document.documentElement.style.width = "100%";
+  }
 
-  alert(
-    `wiped out ${peskyTagCount} pesky elements & left the <article> behind`
-  );
+  function wipePeskyTags() {
+    let peskyTagCount = 0;
 
-  document.createElement();
+    let peskySemanticTags = document.querySelectorAll(
+      "aside, footer, header, #left-sidebar"
+    );
+
+    console.log(peskySemanticTags);
+
+    peskySemanticTags.forEach((element) => {
+      peskyTagCount++;
+      element.remove();
+    });
+
+    alert(
+      `wiped out ${peskyTagCount} pesky elements & left the <article> behind`
+    );
+  }
+
+  if (
+    window.confirm(
+      "try injecting a full-size iframe? this may or may not work."
+    )
+  ) {
+    injectIframe();
+  }
+
+  wipePeskyTags();
 })();
